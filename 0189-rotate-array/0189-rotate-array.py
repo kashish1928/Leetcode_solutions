838e1d091ef4e0a48a6d942b6fac50408d59d6aa
Kashish Joshipura <66333841+kashish1928@users.noreply.github.com>
2025-07-27 22:12:33 -0700

    Time: 3 ms (74.53%), Space: 25.5 MB (92.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if(len(nums)<2):
-            return nums
-        temp = []
-        for i in nums:
-            temp.append(i)
         k = k % len(nums)
-        for i in range(0,len(nums)):
-            nums[i] = temp[i-k]
-            
-        
+        if k == 0:
+            return nums
+        nums[:] = [nums[i-k] for i in range(len(nums))]
 
         
