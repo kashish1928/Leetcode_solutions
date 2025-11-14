f4a1b8a861a361bff56a80852d3d16249a3d088d
Kashish Joshipura <66333841+kashish1928@users.noreply.github.com>
2025-11-13 16:52:06 -0800

    Time: 38 ms (92.49%), Space: 32.5 MB (47.06%) - LeetHub

--- 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.py
+++ 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.py
@@ -1,5 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        num = [abs(x) for x in nums]
-        num.sort(reverse = True)
-        return num[0]*num[1]*10**5
+        max1 = max(abs(nums[0]),abs(nums[1]))
+        max2 = min(abs(nums[0]),abs(nums[1]))
+        for j in nums[2:]:
+            i = abs(j)
+            if(i > max1):
+                max2 = max1
+                max1 = i
+            elif(i > max2):
+                max2 = i
+        return max1*max2*10**5
